b6d588ad47d80cbd0abed69d200bb8b6b8232d83
JerezM <jerez149@gmail.com>
2021-07-24 19:03:43 -0300

    createStudent full functionality added

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -9,4 +9,11 @@ public interface StudentService {
      * @return a collection of all the actual students.
      */
     public List<Student> getStudents();
+
+    /**
+     * Create a new student using the parameterized one.
+     * @param student Student that will be created.
+     * @return The student created.
+     */
+    public Student createStudent(Student student);
 }
