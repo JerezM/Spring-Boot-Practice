590a89f51ff7b564c57d3ebc6979b893dd82052c
JerezM <jerez149@gmail.com>
2021-07-30 17:16:05 -0300

    canDeleteStudent integration test added to StudentIT

--- src/test/java/com/example/demo/integration/StudentIT.java
+++ src/test/java/com/example/demo/integration/StudentIT.java
@@ -1,7 +1,6 @@
 package com.example.demo.integration;
 
 import com.example.demo.student.Student;
-import com.example.demo.student.StudentController;
 import com.example.demo.student.StudentRepository;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -63,16 +62,74 @@ public class StudentIT {
 		String pathRequestMapping = "/api/v1/students";
 
 		// when
-		ResultActions resultActions = mockMvc
+		ResultActions resultActions = this.mockMvc
 			.perform(post(pathRequestMapping)
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(student))
+				.content(this.objectMapper.writeValueAsString(student))
 			)
 		;
 			
 		// then
 		resultActions.andExpect(status().isOk());
-		List<Student> students = studentRepository.findAll();
+		List<Student> students = this.studentRepository.findAll();
 		assertThat(students).usingElementComparatorIgnoringFields("id").contains(student);
 	}
+
+	@Test
+    void canDeleteStudent() throws Exception {
+        // given
+        String name = String.format(
+            "%s %s",
+            faker.name().firstName(),
+            faker.name().lastName()
+        );
+
+        String email = String.format("%s@gmail.com",
+                StringUtils.trimAllWhitespace(name.trim().toLowerCase()));
+
+        Student student = new Student(
+                name,
+                email,
+                LocalDate.now()
+        );
+
+		String pathRequestMapping = "/api/v1/students";
+
+        this.mockMvc.perform(post(pathRequestMapping)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(student)))
+                .andExpect(status().isOk());
+
+        MvcResult getStudentsResult = this.mockMvc.perform(get(pathRequestMapping)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String contentAsString = getStudentsResult
+                .getResponse()
+                .getContentAsString();
+
+        List<Student> students = this.objectMapper.readValue(
+                contentAsString,
+                new TypeReference<>() {
+                }
+        );
+
+        long id = students.stream()
+                .filter(s -> s.getEmail().equals(student.getEmail()))
+                .map(Student::getId)
+                .findFirst()
+                .orElseThrow(() ->
+                        new IllegalStateException(
+                                "student with email: " + email + " not found"));
+
+        // when
+        ResultActions resultActions = this.mockMvc
+                .perform(delete(pathRequestMapping +"/" + id));
+
+        // then
+        resultActions.andExpect(status().isOk());
+        boolean exists = this.studentRepository.existsById(id);
+        assertThat(exists).isFalse();
+    }
 }
